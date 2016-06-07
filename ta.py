f1f6f910cdabe9a0e7f2a84137e2261c4b8696d9
TGMarkiewicz <tomasz.g.markiewicz@gmail.com>
2016-06-07 19:26:49 +0200

    Add EMA

--- ta.py
+++ ta.py
@@ -6,33 +6,103 @@ import math
 import datetime as dt
 
 
-def movingAverage(data, size):
+def SMA(data, size):
     '''
+    SMA - Simple Moving Average
+
     data - array with numerical data
     size - length of the moving average
     '''
 
     lenData = len(data)
-    if size > lenData:
-        raise ValueError('size must be less or equal size of the data')
+    if size > lenData: raise ValueError('size must be less or equal the size of the data')
 
-    result = np.linspace(0, 0, lenData - size + 1)
+    result = np.linspace(0, 0, lenData)
 
-    s = 0.0
+    x = np.concatenate([np.linspace(data[0], data[0], size - 1), data])
 
-    for i in xrange(size):
-        s += data[i]
+    s = sum(x[:size])
+
 
     result[0] = s
 
-    for i in xrange(size, lenData):
-        s -= data[i - size]
-        s += data[i]
+    for i in xrange(size, len(x)):
+        s += x[i] - x[i - size]
         result[i - size + 1] = s
 
     return result/size
 
 
+def CMA(data, size):
+    '''
+    CMA - Cumulative Moving Average
+
+    data - array with numerical data
+    size - length of the moving average
+    '''
+
+    lenData = len(data)
+    if size > lenData: raise ValueError('size must be less or equal the size of the data')
+
+    result = np.linspace(0, 0, lenData)
+
+    s = sum(data[:size])/size
+    result[0:size] = np.linspace(s, s, size) # sets result first 'size' values to s
+
+    for i in xrange(size, lenData):
+        result[i] = (data[i] + i*result[i - 1])/(i + 1.0)
+
+    return result;
+
+def WMA(data, size):
+    '''
+    WMA - Weighted Moving Average
+
+    data - array with numerical data
+    size - length of the moving average
+    '''
+
+    lenData = len(data)
+    if size > lenData: raise ValueError('size must be less or equal the size of the data')
+
+    y = np.linspace(0, 0, lenData)
+
+    x = reduce(lambda x, y: x + y, map(lambda x, y: x*y, data[:size], np.linspace(1, size, size)))
+    y[0:size] = np.linspace(x, x, size)
+
+    T = sum(data[:size])
+
+    for i in xrange(size, lenData):
+        y[i] = y[i - 1] + size*data[i] - T
+        T = T - data[i - size] + data[i]
+
+    D = 0.5*size*(size + 1)
+    return y/D
+
+def EMA(data, size):
+    '''
+    EMA - Exponential (Weighted) Moving Average
+
+    data - array with numerical data
+    size - length of the moving average
+    '''
+
+    lenData = len(data)
+    if size > lenData: raise ValueError('size must be less or equal the size of the data')
+
+    y = np.linspace(0, 0, lenData)
+
+    y[0] = data[0]
+
+    a = 0.5
+    a_ = 1 - a
+
+    for i in xrange(1, lenData):
+        y[i] = a*data[i] + a_*y[i - 1]
+
+    return y
+
+def EWMA(data, size): return EMA(data, size)
 
 def main():
     return 0
--- ta.tests.py
+++ ta.tests.py
@@ -2,23 +2,76 @@ import unittest
 import numpy as np
 import ta
 
-class movingAverageTest(unittest.TestCase):
+class SMA_Test(unittest.TestCase):
     def test_size_greater_that_data_length_throws_error(self):
         with self.assertRaises(ValueError):
-            ta.movingAverage(np.linspace(1, 1, 1), 22)
+            ta.SMA(np.linspace(1, 1, 1), 22)
 
 
     def test_only_ones_in_input_vector(self):
-        inData = np.linspace(1, 1, 10)
-        actual = ta.movingAverage(inData, 3)
-        expected = np.linspace(1, 1, 8)
+        actual = ta.SMA(np.linspace(1, 1, 10), 3)
+        expected = np.linspace(1, 1, 10)
         self.failUnless(len(expected) == len(actual))
         self.failUnless(all(expected == actual))
 
     def test_arithmethatically_increaing_input(self):
-        inData = np.linspace(1, 10, 10)
-        actual = ta.movingAverage(inData, 3)
-        expected = np.linspace(2, 9, 8)
+        actual = ta.SMA(np.linspace(1, 10, 10), 3)
+        expected = np.concatenate([[1, 4.0/3.0], np.linspace(2, 9, 8)])
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+class CMATest(unittest.TestCase):
+    def test_size_greater_than_data_length_throws_errer(self):
+        with self.assertRaises(ValueError):
+            ta.CMA(np.linspace(1, 1, 1), 2)
+
+    def test_only_ones_in_input_vector(self):
+        inData = np.linspace(1, 1, 10)
+        actual = ta.CMA(np.linspace(1, 1, 10), 3)
+        expected = np.linspace(1, 1, 10)
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+    def test_arithmethatically_increaing_input(self):
+        actual = ta.CMA(np.linspace(1, 10, 10), 3)
+        expected = np.concatenate([[2, 2], np.linspace(2, 5.5, 8)])
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+class WMATest(unittest.TestCase):
+    def test_size_greater_than_data_length_throws_errer(self):
+        with self.assertRaises(ValueError):
+            ta.WMA(np.linspace(1, 1, 1), 2)
+
+    def test_only_ones_in_input_vector(self):
+        inData = np.linspace(1, 1, 10)
+        actual = ta.WMA(np.linspace(1, 1, 10), 3)
+        expected = np.linspace(1, 1, 10)
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+    def test_arithmethatically_increaing_input(self):
+        actual = ta.WMA(np.linspace(1, 10, 10), 3)
+        expected = np.concatenate([[14/6.0, 14/6.0], np.linspace(14, 56, 8)/6.0])
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+class EMATest(unittest.TestCase):
+    def test_size_greater_than_data_length_throws_errer(self):
+        with self.assertRaises(ValueError):
+            ta.EMA(np.linspace(1, 1, 1), 2)
+
+    def test_only_ones_in_input_vector(self):
+        inData = np.linspace(1, 1, 10)
+        actual = ta.EMA(np.linspace(1, 1, 10), 3)
+        expected = np.linspace(1, 1, 10)
+        self.failUnless(len(expected) == len(actual))
+        self.failUnless(all(expected == actual))
+
+    def test_arithmethatically_increaing_input(self):
+        actual = ta.WMA(np.linspace(1, 10, 10), 3)
+        expected = np.concatenate([[14/6.0, 14/6.0], np.linspace(14, 56, 8)/6.0])
+        return
         self.failUnless(len(expected) == len(actual))
         self.failUnless(all(expected == actual))
 
